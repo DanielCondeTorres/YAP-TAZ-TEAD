dc9878e7a5235503ed075b5c2141831109138ee5
Dani <you@example.com>
2025-02-04 17:59:35 +0000

    clean all

--- Code/Working_Area/production.sh
+++ Code/Working_Area/production.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=4G
 
-module load cesga/2020 gcc/system openmpi/4.1.4_ft3 gromacs/2024.2-cuda-system
+module load cesga/2022 gcc/system openmpi/4.1.4 gromacs/2024.2-PLUMED-2.9.2
 
 #srun gmx_mpi mdrun -pin on -cpi -noappend -s ../Output/prod.tpr
 srun gmx_mpi mdrun -pin on -cpi ../Output/state.cpt -s ../Output/prod.tpr -o ../Output/traj.xtc -g ../Output/md.log -c ../Output/confout.gro #-cpi ../Output/state.cpt
